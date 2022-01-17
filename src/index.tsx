0e2e41ba4fe39ec0d5c746b12396960ab6008b8a
Connor Shearer <connor_shearer@outlook.com>
2022-01-17 21:59:51 +0000

    Revert "Add in default React PWA typescript"
    This reverts commit 36c234b526b0155d596df46f261c3053e629e1cc.

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import { ChakraProvider, extendTheme } from "@chakra-ui/react"
 import '@fontsource/exo'
 
@@ -33,5 +32,7 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-serviceWorkerRegistration.register();
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
