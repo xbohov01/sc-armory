1834140548ad56f10963e8102e68dcc8af09f929
SB <samo.bohovic@gmail.com>
2022-04-21 13:52:13 +0200

    fix items with no sale location

--- src/components/shoppingList/ShoppingLocation.tsx
+++ src/components/shoppingList/ShoppingLocation.tsx
@@ -1,4 +1,7 @@
-import { Box, VStack } from "@chakra-ui/layout";
+import { AiFillQuestionCircle } from "react-icons/ai";
+
+import { Box, HStack, VStack } from "@chakra-ui/layout";
+import { Tooltip } from "@chakra-ui/react";
 
 import { KeyValue, ListKey, LocatedItem } from "../../types/types";
 
@@ -10,6 +13,25 @@ type ShoppingLocationProps = {
 };
 
 export default function ShoppingLocation(props: ShoppingLocationProps) {
+  const buildLocationToolTip = () => {
+    return (
+      <Tooltip 
+        placement="auto" 
+        label={props.items.key.name === 'N/A' ? 'Possible loot only item(s)' : ''}
+      >
+        <HStack>
+          <>
+            {`${props.items.key.name} - ${props.items.key.chain
+              .split("-")
+              .reverse()
+              .join(" - ")}`}
+          </>
+          <>{props.items.key.name === 'N/A' ? <AiFillQuestionCircle /> : ""}</>
+        </HStack>
+      </Tooltip>
+    );
+  };
+
   return (
     <Box
       flex="1"
@@ -20,13 +42,10 @@ export default function ShoppingLocation(props: ShoppingLocationProps) {
       backgroundColor="#1a2130"
     >
       <Box flex="1" textAlign="left" fontSize="large" paddingBottom="10pt">
-        <p>{`${
+        {`${
           props.items.value.filter((i) => !i.isBought).length
-        } of remaining items`}</p>
-        <p>{`${props.items.key.name} - ${props.items.key.chain
-          .split("-")
-          .reverse()
-          .join(" - ")}`}</p>
+        } of remaining items`}
+        {buildLocationToolTip()}
       </Box>
       <VStack>
         {props.items.value.map((i) => (
